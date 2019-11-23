e00a6b55d4d2360d1d7055e7a14fd0b8c7a50dbf
sandyhery <sandyhery1115@gmail.com>
2019-11-23 20:16:38 +0800

    Update KuWoMusicDownload.js

--- Surge/JS/KuWoMusicDownload.js
+++ Surge/JS/KuWoMusicDownload.js
@@ -2,12 +2,16 @@
 KuWo music unlock lossless download, need to use with KuWo music VIP script. this script is only compatible with surge 4.0
 
 Surge4.0:
-http-request https?:\/\/musicpay\.kuwo.cn\/music\.pay\?uid=\d+ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/KuWoMusicDownload.js
+http-request https?:\/\/musicpay\.kuwo.cn\/music\.pay\?uid=\d+ script-path=https://raw.githubusercontent.com/sandyhery/Script/master/Surge/JS/KuWoMusicDownload.js
 #Vip script:
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/sandyhery/Script/master/Surge/JS/Kuwo.js
 
 MITM = vip1.kuwo.cn
 */
+
+/**
+ * @supported 你的设备ID F4C597C9913A
+ */
 
 let url = $request.url.replace(/uid=\d+/g, "uid=1");
 $done({url});
